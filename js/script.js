da34f4576b452b11343ad8da112358db4802714b
kevincsimplon <yeet@live.fr>
2016-01-04 11:23:02 +0100

    final

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#browser").text(window.navigator.userAgent);
+
+$(window).on('resize load', function(event) {
+      $("#width").text(window.innerWidth);
+      $("#height").text(window.innerHeight);
+});
+});
